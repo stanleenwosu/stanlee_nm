e21a0e03f3b2c4af35f9ccc75d9bb45d8eef35f9
stanleenwosu <stanleenwosu@yahoo.com>
2026-09-01 09:29:51 +0100

    base for stanlee_nm

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/stanlee_nm/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
